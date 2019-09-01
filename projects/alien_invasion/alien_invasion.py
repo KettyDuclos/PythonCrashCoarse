bdc3e8d51f47079075633f2fe2a047d2676daaf5
Ketty Duclos <ketty.duclos11@gmail.com>
2019-09-01 10:00:47 -0400

    created ship class, drew ship to screen

---
+++ projects/alien_invasion/alien_invasion.py
@@ -0,0 +1,28 @@
+import sys
+import pygame
+from ship import Ship
+import game_functions as gf
+from settings import Settings
+
+
+def run_game():
+    #initialize pygame, settings, and screen object
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption('Alien Invasion')
+
+    #make a ship
+    ship = Ship(screen)
+
+    #Start the main loop for the game.
+    while True:
+
+        #Watch for keyboard and mouse events.
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
+
+        
+run_game()
+
