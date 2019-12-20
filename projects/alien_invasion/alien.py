eb6a98dafdcabf98a088bac4c58a39730136bebe
Ketty Duclos <ketty.duclos11@gmail.com>
2019-12-20 11:00:25 -0500

    add

--- projects/alien_invasion/alien.py
+++ projects/alien_invasion/alien.py
@@ -7,3 +7,26 @@ class Alien(Sprite):
 
          super(Bullet, self).__init__()
         self.screen = screen
+
+
+# def __init__(self, ai_settings, screen, ship):
+        """Create a bullet object at the ship's current position"""
+        super(Bullet, self).__init__()
+        self.screen = screen
+
+        #Create a bullet rect at (0,0) and then set correct position.
+        self.rect = pygame.Rect(0,0,ai_settings.bullet_width,
+            ai_settings.bullet_height)
+        self.rect.centerx = ship.rect.centerx
+        self.rect.top = ship.rect.top
+
+        #Store the bullet's position as a decimal value
+        self.y = float(self.rect.y)
+
+        self.color = ai_settings.bullet_color
+        self.speed_factor = ai_settings.bullet_speed_factor
+
+    def update(self):
+         """Move the bullet up the screen."""
+         #Update the decimal position of t
+
